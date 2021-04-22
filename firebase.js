985e6b849c59f3fed9e19d8f389aa920e1bde19c
TendedKibbles0 <tendedkibbles0@gmail.com>
2021-04-22 16:00:59 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,4 +3,11 @@
 <script src="/__/firebase/8.4.1/firebase-app.js"></script>
 <script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
 <script src="/__/firebase/init.js"></script>
+{
+  "hosting": {
+    "site": "principle",
+    "public": "public",
+    ...
+  }
+}
 <body>
