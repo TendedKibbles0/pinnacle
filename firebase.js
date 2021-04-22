81ed5a831bf3d43c15274c900724c00b41552dff
TendedKibbles0 <tendedkibbles0@gmail.com>
2021-04-22 15:56:14 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,6 @@
+<head>
+<body>
+<script src="/__/firebase/8.4.1/firebase-app.js"></script>
+<script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
+<script src="/__/firebase/init.js"></script>
+<body>
